eb112e40479a7f3842bc977605a03456ee989487
Fadafacka <148792379+Fadafacka@users.noreply.github.com>
2025-07-12 23:09:52 +0200

    New approach
    Parenthesis inside parenthesis should be added to the the variable.
    Operators are not updated (o +=1)

--- calculator.py
+++ calculator.py
@@ -2,30 +2,26 @@ from tkinter import *
 from tkinter import ttk
 
 def main():
-    calc = calculator()
-    vars = calc.variables
-    ops = calc.operations
-    paren = calc.parenthesis
+    calc    = calculator()
+    vars    = calc.variables
+    ops     = calc.operations
+    paren   = calc.parenthesis
 
     print(f'paren = {paren}')
-    for i in paren:
-        print(i, paren[i])
+    for i in paren : print(i, paren[i])
 
     print(f'\nvars = {vars}')
-    for i in vars:
-        print(i, vars[i])
+    for i in vars : print(i, vars[i])
 
     print(f'\nops = {ops}')    
-    for i in ops:
-        print(i, ops[i])
+    for i in ops : print(i, ops[i])
 
     '''
     
     '''
-    
 
 class calculator:
-    def __init__(self, expr=''):
+    def __init__(self, expr='') :
         if expr == '': expr = input ('Enter the operation: ')
         if expr == '': self.__init__()
 
@@ -39,6 +35,23 @@ class calculator:
         o = 0
 
         for i in range(len(expr)) :
+            print(self._variables)
+            if expr[i] == '(' :
+                #self._parenthesis[expr[i]] = {o : self._parenthesis.get(expr[i], {}).get(o, 0) + 1}
+                count_paren[expr[i]] = count_paren.get(expr[i], 0) + 1
+                print(f'i = {expr[i]}')
+                continue
+
+            if expr[i] == ')' :
+                #self._parenthesis[expr[i]] = {o : self._parenthesis.get(expr[i], {}).get(o, 0) + 1}
+                count_paren[expr[i]] = count_paren.get(expr[i], 0) + 1
+                print(f'i = {expr[i]}')
+                continue
+
+            if count_paren.get('(', 0) > count_paren.get(')', 0) :
+                self._variables[v] = self._variables.get(v, '') + expr[i]
+                continue
+
             if expr[i].isdigit() or (expr[i] == '.') : 
                 self._variables[v] = self._variables.get(v, '') + expr[i]
                 if self._operations.get(o) :
@@ -51,19 +64,12 @@ class calculator:
                     self.__init__()
                 else :
                     v +=1
-                
-# PAY ATTENTION HEREEEE
-            elif expr[i] == '(' :
-                #self._parenthesis[expr[i]] = {o : self._parenthesis.get(expr[i], {}).get(o, 0) + 1}
-                count_paren[expr[i]] = count_paren.get(expr[i], 0) + 1
-
-            elif expr[i] == ')' :
-                #self._parenthesis[expr[i]] = {o : self._parenthesis.get(expr[i], {}).get(o, 0) + 1}
-                count_paren[expr[i]] = count_paren.get(expr[i], 0) + 1                
-
+                    
             else :
                 print(f'Character "{expr[i]}" at position {i+1} is not understood by this calculator\n')
                 self.__init__()
+        print(self._variables)
+        print(self._operations)
 
     	# Error checking
         if count_paren.get('(', 0) > count_paren.get(')', 0) :
@@ -72,7 +78,7 @@ class calculator:
         if count_paren.get('(', 0) < count_paren.get(')', 0) :
             print('Missing opening parenthesis\n')
             self.__init__()
-        if not len(self._variables) == len(self._operations)+1 :
+        if not len(self._variables) == len(self._operations) + 1 :
             print('Operator without operand\n')
             self.__init__()
 
@@ -80,52 +86,52 @@ class calculator:
         for i in self._variables :
             try : self._variables[i] = float(self._variables[i])
             except : continue
-
+        '''
         # Perform calculations
         i = 0
-        while i < len(self._variables)-1 :
+        while i < len(self._variables) - 1 :
             result = self.calculate(self._variables[i], self._variables[i+1], self._operations[i])
             print(result)
             i +=1
-
+        '''
         #self.__init__()
 
     # Variables
     @property
-    def variables(self):
+    def variables(self) :
         return self._variables
     @variables.setter
-    def variables(self, v):
+    def variables(self, v) :
         self._variables = v
 
     # Operations
     @property
-    def operations(self):
+    def operations(self) :
         return self._operations
     @operations.setter
-    def operations(self, o):
+    def operations(self, o) :
         self._operations = o
 
     # Parenthesis
     @property
-    def parenthesis(self):
+    def parenthesis(self) :
         return self._parenthesis
     @parenthesis.setter
-    def parenthesis(self, p):
+    def parenthesis(self, p) :
         self._parenthesis = p
 
 
-    def calculate(self, l, r, op):
-        if op == '*':
+    def calculate(self, l, r, op) :
+        if op == '*' :
             return(l*r)
-        if op == '/':
+        if op == '/' :
             return(l/r)
-        if op == '+':
+        if op == '+' :
             return(l+r)
-        if op == '-':
+        if op == '-' :
             return(l-r)
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     main()
 
 
